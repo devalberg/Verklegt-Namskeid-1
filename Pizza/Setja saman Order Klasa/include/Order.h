44d8c65fcd2c5ba4d66d45cba022935898698063
saemundur <34029692+saemundur@users.noreply.github.com>
2017-11-29 22:02:18 +0000

    Bara vinna

--- Pizza/Setja saman Order Klasa/include/Order.h
+++ Pizza/Setja saman Order Klasa/include/Order.h
@@ -1,14 +1,17 @@
 #ifndef ORDER_H
 #define ORDER_H
 #include "Pizza.h"
-#include "Drink.h"
 #include "Side.h"
+#include "Drink.h"
+
 
 
 class Order
 {
     public:
         Order();
+        void add_pizza(Pizza current_pizza);
+        friend ostream &operator << (ostream& out, Order& current_order);
 
 
     private:
@@ -17,6 +20,8 @@ class Order
         vector <Drink> drinks_list;
         char status;
         double total_price;
+        int id_num;
+        bool dilivery;
 
 };
 
--- Pizza/Setja saman Order Klasa/include/Pizza.h
+++ Pizza/Setja saman Order Klasa/include/Pizza.h
@@ -1,21 +1,28 @@
 #ifndef PIZZA_H
 #define PIZZA_H
-#include <vector>
 #include "Topping.h"
-#include <iostream>
-
-using namespace std;
 
 class Pizza
 {
     public:
         Pizza();
-        add_topping();
+        Pizza(vector<Topping> _all_toppings, int _pizza_size, bool _cheesy_crust);
+        void add_topping(Topping current_topping);
+        bool is_on_menu();
+        bool is_cheesy_crust();
+        bool onoffmenu(bool onoff);
+        friend ostream &operator << (ostream& out,  Pizza& current_pizza);
+
 
     private:
         vector<Topping> all_toppings;
+        string name;
+        bool cheesy_crust;
+        bool on_menu;
         int number_of_toppings;
         int pizza_size;
+
+
 };
 
 #endif // PIZZA_H
